d32cf7e3a000a359ba28a200fdb9b3dc09fac924
Brandon Holt <bholt@cs.washington.edu>
2015-12-10 10:31:17 -0800

    add script with a bunch of lines to type into repl

---
+++ manual_test.scala
@@ -0,0 +1,22 @@
+import owl._
+import Util._
+import com.websudos.phantom.dsl._
+
+val arthur = User(username = "tealuver", name = "Arthur Dent")
+val ford = User(username = "hastowel", name = "Ford Prefect")
+val zaphod = User(username = "froodyprez", name = "Zaphod Beeblebrox")
+
+object ReplService extends OwlService
+import ReplService._
+
+await(service.createTables)
+await(Future.sequence(List(arthur, ford, zaphod) map (service.store(_))))
+
+await(service.follow(arthur.id, zaphod.id))
+await(service.follow(ford.id, zaphod.id))
+await(service.follow(ford.id, arthur.id))
+
+val t1 = Tweet(user = arthur.id, body = "Nutri-matic drinks are the worst. #fml")
+val t2 = Tweet(user = zaphod.id, body = "Things more important than my ego: none")
+await(service.post(t1))
+await(service.post(t2))

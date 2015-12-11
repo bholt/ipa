3a3498ff999cf98745e40948c19d18d6ffa5175b
Brandon Holt <bholt@cs.washington.edu>
2015-12-10 16:31:57 -0800

    reorder

--- src/test/resources/manual_test.scala
+++ src/test/resources/manual_test.scala
@@ -1,13 +1,15 @@
 import owl._
 import Util._
 import com.websudos.phantom.dsl._
+object ReplService extends OwlService
+import ReplService._
 
 val arthur = User(username = "tealuver", name = "Arthur Dent")
 val ford = User(username = "hastowel", name = "Ford Prefect")
 val zaphod = User(username = "froodyprez", name = "Zaphod Beeblebrox")
 
-object ReplService extends OwlService
-import ReplService._
+val t1 = Tweet(user = arthur.id, body = "Nutri-matic drinks are the worst. #fml")
+val t2 = Tweet(user = zaphod.id, body = "Things more important than my ego: none")
 
 await(service.createTables)
 await(Future.sequence(List(arthur, ford, zaphod) map (service.store(_))))

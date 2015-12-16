cef38c51c1066681b12931c15948631b622e612f
Brandon Holt <bholt@cs.washington.edu>
2015-12-15 18:40:01 -0800

    add helpers: `weightedSample` & `.bundle` implicit

--- src/main/scala/owl/Util.scala
+++ src/main/scala/owl/Util.scala
@@ -1,9 +1,8 @@
 package owl
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future, Await}
 import scala.concurrent.duration.Duration
 import scala.util.Random
-import scala.concurrent.Await
 
 object Util {
 
@@ -11,6 +10,30 @@ object Util {
     def sample = v(Random.nextInt(v.length))
   }
 
+  implicit class FutureSeqPlus[T](v: Iterator[Future[T]]) {
+    def bundle(implicit ec: ExecutionContext) = Future.sequence(v)
+  }
+
+  /**
+    * Weighted random sample keys from map.
+    * http://stackoverflow.com/a/24869852
+    */
+  final def weightedSample[A](dist: Map[A, Double]): A = {
+    val p = scala.util.Random.nextDouble
+    val it = dist.iterator
+    var accum = 0.0
+    while (it.hasNext) {
+      val (item, itemProb) = it.next
+      accum += itemProb
+      if (accum >= p)
+        return item  // return so that we don't have to search through the whole distribution
+    }
+    sys.error(f"this should never happen")  // needed so it will compile
+  }
+
+  /**
+    * Helper to just block waiting for result of a Future.
+    */
   def await[T](f : Future[T]): T = Await.result(f, Duration.Inf)
 
 }

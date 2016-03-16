eecdc397a64a772769961285a07f418aa3eb66d6
Brandon Holt <bholt@cs.washington.edu>
2016-03-16 14:28:55 -0700

    measure consistency of RawMixCounter

--- src/main/scala/owl/RawMixCounter.scala
+++ src/main/scala/owl/RawMixCounter.scala
@@ -1,6 +1,10 @@
 package owl
 
-import java.util.concurrent.Semaphore
+import java.util.UUID
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicInteger, AtomicLong}
+import java.util.concurrent.locks.{Lock, ReentrantLock, ReentrantReadWriteLock}
+import java.util.concurrent.{ConcurrentHashMap, Semaphore}
+import java.util.function.Function
 
 import com.websudos.phantom.connectors.KeySpace
 import com.websudos.phantom.dsl._
@@ -8,6 +12,7 @@ import ipa.IPACounter
 import org.apache.commons.math3.distribution.ZipfDistribution
 import owl.Util._
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.util.Random
 
@@ -15,6 +20,7 @@ class RawMixCounter(val duration: FiniteDuration) extends {
   override implicit val space = RawMix.space
 } with OwlService {
   import Consistency._
+  import RawMixCounter.truth
 
   val nsets = config.rawmix.nsets
   val mix = config.rawmix.counter.mix
@@ -32,19 +38,29 @@ class RawMixCounter(val duration: FiniteDuration) extends {
   val countReadStrong = metrics.create.counter("read_strong")
   val countReadWeak   = metrics.create.counter("read_weak")
 
-  val countConsistent = metrics.create.counter("consistent")
-  val countInconsistent = metrics.create.counter("inconsistent")
-
   val histIntervalWidth = metrics.create.histogram("interval_width")
+  val countCorrect = metrics.create.counter("correct")
+  val countIncorrect = metrics.create.counter("incorrect")
+  val histError = metrics.create.histogram("error")
+  val countErrorNegative = metrics.create.counter("error_negative")
 
-  def recordResult(r: Inconsistent[Long]): Inconsistent[Long] = {
+  def recordResult(trueVal: Long, inflight: Long, r: Inconsistent[Long]): Inconsistent[Long] = {
+
     // some require special additional handling...
-    counter match {
-      case _: IPACounter.ErrorTolerance =>
-        val iv = r.asInstanceOf[Interval[Long]]
-        val width = iv.max - iv.min
+    r match {
+      case v: Interval[Long] =>
+        val width = v.max - v.min
         histIntervalWidth << width
-      case _ => // do nothing
+        if (v.contains(trueVal)) countCorrect += 1 else countIncorrect += 1
+        histError << Math.abs(v.median - trueVal)
+        if (v.median < trueVal) countErrorNegative += 1
+      case v =>
+        if (v.get >= trueVal && v.get <= (trueVal+inflight)) countCorrect += 1
+        else countIncorrect += 1
+
+        histError << Math.abs(v.get - trueVal)
+        histIntervalWidth << inflight
+        if (v.get < trueVal) countErrorNegative += 1
     }
     r.consistency match {
       case Strong => countReadStrong += 1
@@ -67,11 +83,18 @@ class RawMixCounter(val duration: FiniteDuration) extends {
       sem.acquire()
       val handle = counter(zipfID())
       val op = weightedSample(mix)
+      val t = truth(handle.key)
       val f = op match {
         case 'incr =>
+          t.inflight.incrementAndGet()
           handle.incr().instrument(timerIncr)
+                .map { _ => t.incr(); t.inflight.decrementAndGet() }
         case 'read =>
-          handle.read().instrument(timerRead).map(recordResult(_)).unit
+          val t = truth(handle.key)
+          val trueVal = t.get
+          val f = t.inflight.get
+          handle.read().instrument(timerRead)
+              .map(recordResult(trueVal, Math.max(t.inflight.get, f), _)).unit
       }
       f onSuccess { case _ => sem.release() }
       f onFailure { case e: Throwable =>
@@ -90,6 +113,20 @@ class RawMixCounter(val duration: FiniteDuration) extends {
 object RawMixCounter extends {
   override implicit val space = KeySpace("rawmix")
 } with Connector {
+
+  case class Truth(v: AtomicLong = new AtomicLong,
+      inflight: AtomicLong = new AtomicLong) {
+    def incr() = v.incrementAndGet()
+    def get = v.get()
+  }
+
+
+  val truthValues = new ConcurrentHashMap[UUID, Truth]()
+  def truth(key: UUID): Truth = {
+    truthValues.computeIfAbsent(key, new Function[UUID,Truth] {
+      override def apply(key: UUID) = Truth()
+    })
+  }
 
   def main(args: Array[String]): Unit = {
     if (config.do_reset) dropKeyspace()
